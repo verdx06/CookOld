c226af9293d9fb9200761c90adf3fbec15ea528b
Виталий Багаутдинов <vitalijbagautdinov@MacBook-Air-Vitalij.local>
2026-04-01 16:32:38 +0300

    Добавил пустые экраны

---
+++ Sources/Presentation/Models/Favorite/FavoriteView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteView.swift
+//  CulinarApp
+//
+//  Created by Виталий Багаутдинов on 01.04.2026.
+//
+
+import SwiftUI
+
+struct FavoriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteView()
+}
---
+++ Sources/Presentation/Models/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  CulinarApp
+//
+//  Created by Виталий Багаутдинов on 01.04.2026.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
