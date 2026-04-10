66ed662e22e592e65f50d8980815cf4f0e3f7ed0
eventya <eventya@MacBook-Pro-eventya-2.local>
2026-04-10 11:24:11 +0300

    resolve conflict: remove seed methods from SwiftDataFavouritesRepository

--- Sources/Data/Persistence/SwiftDataFavouritesRepository.swift
+++ Sources/Data/Persistence/SwiftDataFavouritesRepository.swift
@@ -56,59 +56,6 @@ final class SwiftDataFavouritesRepository: FavouritesRepository {
         try? context.save()
     }
 
-    // MARK: - Seed
-
-    func resetSeed() {
-        let all = (try? context.fetch(FetchDescriptor<FavouriteModel>())) ?? []
-        all.forEach { context.delete($0) }
-        try? context.save()
-        seedIfEmpty()
-    }
-
-    func seedIfEmpty() {
-        guard fetchAll().isEmpty else { return }
-
-        let meals: [Meal] = [
-            Meal(
-                idMeal: "52772",
-                strMeal: "Teriyaki Chicken Casserole",
-                strMealThumb: "https://www.themealdb.com/images/media/meals/wvpsxx1468256321.jpg",
-                strArea: "Japanese",
-                strCategory: "Chicken"
-            ),
-            Meal(
-                idMeal: "53049",
-                strMeal: "Croissant aux Amandes",
-                strMealThumb: "https://www.themealdb.com/images/media/meals/y5ruu61614080982.jpg",
-                strArea: "French",
-                strCategory: "Dessert"
-            ),
-            Meal(
-                idMeal: "52965",
-                strMeal: "Spaghetti Bolognese",
-                strMealThumb: "https://www.themealdb.com/images/media/meals/sutysw1468247559.jpg",
-                strArea: "Italian",
-                strCategory: "Pasta"
-            ),
-            Meal(
-                idMeal: "52834",
-                strMeal: "Poutine",
-                strMealThumb: "https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg",
-                strArea: "Canadian",
-                strCategory: "Miscellaneous"
-            ),
-            Meal(
-                idMeal: "52856",
-                strMeal: "Shakshuka",
-                strMealThumb: "https://www.themealdb.com/images/media/meals/g373701551450225.jpg",
-                strArea: "Tunisian",
-                strCategory: "Breakfast"
-            )
-        ]
-
-        meals.forEach { save($0) }
-    }
-
     // MARK: - Helpers
 
     private func find(_ id: String) -> FavouriteModel? {
